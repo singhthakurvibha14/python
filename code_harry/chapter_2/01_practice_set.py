73d6641575cc897eedfd4a8fec39e8cb77258baf
Vibha Singh Thakur <singhthakurvibha14@gmail.com>
2026-05-02 12:42:22 +0530

    Update code

---
+++ code_harry/chapter_2/01_practice_set.py
@@ -0,0 +1,38 @@
+ #problem 01 - sum of 2 element
+
+# a=23
+# b=17
+# print("sum : " , a+b)
+
+#problem 02 - reminder
+
+# a = int(input("enter the num : "))
+# b = int(input("enter b :"))
+# c=a % b
+# print(c)
+
+ #problem 03 = greater or not 
+
+# a = 34
+# b = 80 
+# print(a>b)
+
+#problem 04 = average of number
+
+# a = int(input("enter a :"))
+# b = int(input("enter b :"))
+# c=(a+b)/2
+# print(c)
+
+#problem 05 = square of number
+
+# a=int(input("enter a : "))
+# print(a*a)
+
+#problem 06 = check type 
+
+a=input("enter a :")
+print(type(a))
+
+
+
---
+++ code_harry/chapter_2/input.py
@@ -0,0 +1,8 @@
+a = input("enter a : ")
+b = input("enter b : ")
+print("sum : " , a+b) #here the answer come as 12 because a and b work as string and they concate each other
+
+# so to handle this we do typecasting
+d = int(input("enter c : "))
+c = int(input("enter c : "))
+print("sum : " , c+d)
